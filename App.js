053dda91d520237ac2c6e9cf55d9989fb2647f43
Guilherme Mendes Barnabe <guilherme.barnabe@grupointegrado.br>
2024-02-22 20:22:24 -0300

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={() => alert('Hello, world!')}>
+        <Text>Press me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
