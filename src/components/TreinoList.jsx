ed8e974feef2593359172c0d500b8ecab9922270
Guilherme Mendes Barnabe <guilherme.barnabe@grupointegrado.br>
2024-06-12 20:43:19 -0300

    Add paddingTop to TreinoList component for better spacing

--- src/components/TreinoList.jsx
+++ src/components/TreinoList.jsx
@@ -156,6 +156,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#232323",
+    paddingTop: 50,
     width: "100%",
   },
   backButton: {
